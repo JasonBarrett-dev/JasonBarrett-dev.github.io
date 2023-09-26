e5e570163b7f0224e49dc86a7aae266b1f83f6b4
JasonBarrett1998 <jason.barrett.55@gmail.com>
2023-09-26 20:53:34 +0100

    static adapter / github pages configuration

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,20 @@ import {
     vitePreprocess
 } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
 
     kit: {
-        adapter: adapter()
+        adapter: adapter({
+            // TODO fallback: 'index.html'
+        }),
+		paths: {
+			// For Github Pages we need to serve the website from a folder with the same name as the repository.
+            base: dev ? '' : process.env.BASE_PATH,
+		},
     }
 };
 
